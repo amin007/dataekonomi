885d8a5a78cfaeb5cf5ce04e556764ff6198213c
Amin007 - Server <amin800507@gmail.com>
2015-09-13 09:02:52 +0800

    buang fungsi yang tidak perlu wujud dalam KAWAL

--- aplikasi/tanya/semakan_tanya.php
+++ aplikasi/tanya/semakan_tanya.php
@@ -16,7 +16,7 @@ class Semakan_Tanya extends Tanya
 			'newss' : 'sidap'; 
 		$cariID = ( !isset($cari['id']) ) ? '' : $cari['id'];
 
-		$sql = "SELECT * FROM $myTable "
+		$sql = "SELECT $medan FROM $myTable "
 			 . "WHERE $cariMedan LIKE '%$cariID%' ";
 		
 		//echo '<hr><pre>cariKes()=>$sql='; print_r($sql) . '</pre>';
@@ -46,40 +46,11 @@ class Semakan_Tanya extends Tanya
 		return $data;
 	}
 
-	public function cariProdukLama($myTable, $medan, $cari)
-	{
-		$sv = ( !isset($cari['sv']) ) ? '' : $cari['sv'];
-		$cariMedan = ( !isset($cari['medan']) ) ? '' : $cari['medan'];
-		$cariID = ( !isset($cari['id']) ) ? '' : $cari['id'];
-		$thnMula = ( !isset($cari['thn_mula']) ) ? '' : $cari['thn_mula'];
-		$thnAkhir = ( !isset($cari['thn_akhir']) ) ? '' : $cari['thn_akhir'];
-		
-		$sql = "SELECT $medan FROM `$myTable` "
-			 . "WHERE $cariMedan like '$cariID%' ";
-		$sql .= ($sv=='205') ? 
-			"AND thn BETWEEN $thnMula and $thnAkhir " : '';
-		
-		//echo '<hr><pre>cariProdukLama()=>$sql='; print_r($sql) . '</pre>';
-		$data = $this->db->selectAll($sql,2);
-		//echo '<hr><pre>$data='; print_r($data) . '</pre>';
-		
-		return $data;
-	}
-
 	public function cariProdukBaru($myTable, $sql)
 	{
 		//echo '<hr><pre>cariProdukbaru()=>$sql='; print_r($sql) . '</pre>';
 		$data = $this->db->selectAll($sql,2);
 		//echo '<hr><pre>cariProdukbaru()$data='; print_r($data) . '</pre>';
-		
-		return $data;
-	}
-
-	public function cariAset($myTable, $sql)
-	{
-		//echo '<hr><pre>cariProduk()=>$sql='; print_r($sql) . '</pre>';
-		$data = $this->db->selectAll($sql,2);
-		//echo '<hr><pre>$data='; print_r($data) . '</pre>';
 		
 		return $data;
 	}
@@ -101,7 +72,8 @@ class Semakan_Tanya extends Tanya
 		
 		return $data;
 	}
-
+##########################################################################################
+## fungsi standard setiap class *_Tanya
 	public function paparMedan($myTable, $papar = null)
 	{
 		$cari = ( !isset($papar) ) ? '' : ' WHERE  ' . $papar . ' ';
@@ -111,21 +83,7 @@ class Semakan_Tanya extends Tanya
 		//echo $sql . '<br>';
 		return $this->db->selectAll($sql);
 	}
-
-	public function paparSemua($sv, $myTable, $medan, $fe, $jum)
-	{
-		//$jum['dari'] . ', ' . $jum['max']
-		$carife = ( !isset($fe) ) ? '' : ' WHERE fe = "' . $fe . '"';
-		$sql = 'SELECT ' . $medan . ' FROM ' . 
-		$sv . $myTable . ' b ' . $carife . $this->_susun;
-		
-		//echo $sql . '<br>';
-		$result = $this->db->selectAll($sql);
-		//echo json_encode($result);
-		
-		return $result;
-	}
-		
+	
 	public function cariSemuaMedan($myTable, $medan, $cari)
 	{
 		$cariMedan = ( !isset($cari['medan']) ) ? '' : $cari['medan'];
@@ -149,19 +107,16 @@ class Semakan_Tanya extends Tanya
 		
 		foreach ($data as $medan => $nilai)
 		{
-			//$postData[$medan] = $nilai;
 			if ($medan == $medanID)
 				$cariID = $medan;
 			elseif ($medan != $medanID)
 				$senarai[] = ($nilai==null) ? " `$medan`=null" : " `$medan`='$nilai'"; 
-			if(($medan == 'fe'))
-				$fe = ($nilai==null) ? " `$medan`=null" : " `$medan`='$nilai'"; 
 		}
 		
 		$senaraiData = implode(",\r",$senarai);
 		$where = "`$cariID` = '{$data[$cariID]}' ";
 		
-		// set sql
+		# set sql
 		$sql = " UPDATE `$myTable` SET \r$senaraiData\r WHERE $where";
 		echo '<pre>$sql->', print_r($sql, 1) . '</pre>';
 		//$this->db->update($sql);
@@ -182,7 +137,7 @@ class Semakan_Tanya extends Tanya
 		
 		$senaraiData = implode(",\r",$senarai);
 		
-		// set sql
+		# set sql
 		$sql = " INSERT `$myTable` SET \r$senaraiData";
 		//echo '<pre>$sql->', print_r($sql, 1) . '</pre>';
 		$this->db->insert($sql);

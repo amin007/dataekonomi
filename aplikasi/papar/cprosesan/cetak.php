5e2643e22e97270283ab02900cd0531db86fc1c6
Amin007 - Server <amin800507@gmail.com>
2015-09-27 09:48:45 +0800

    tambah $ID dan paparkan jadual2 tertentu sahaja

--- aplikasi/papar/cprosesan/cetak.php
+++ aplikasi/papar/cprosesan/cetak.php
@@ -19,7 +19,7 @@ else
 <span class="badge"><?php echo $tajuk ?></span>
 <a href="<?php echo $cetak ?>"><span class="badge">
 <i class="glyphicon glyphicon-file"></i>Papar</span></a>
-<input type="text" name="cari" size="40" value="<?=$ID?>">
+<input type="text" name="cari" size="40" value="<?php echo $ID ?>">
 <font size="5" color="red">&rarr;</font>
 <input type="submit" value="mencari">
 </form></div>
@@ -33,17 +33,17 @@ $soalAm = array(
 	's'.$this->sv.'_q05_2010',
 	's'.$this->sv.'_q05a_2010',
 	's'.$this->sv.'_q05b_2010',
-	's'.$this->sv.'_q10_2010',
 	's'.$this->sv.'_q11_2010',
-/*	's'.$this->sv.'_qsd_2010',
+	/*'s'.$this->sv.'_q10_2010',
+	's'.$this->sv.'_qsd_2010',
 	's'.$this->sv.'_q06_2010',
 	's'.$this->sv.'_q07_2010',
-	's'.$this->sv.'_qsa_2010',*/
+	's'.$this->sv.'_qsa_2010',
 	's'.$this->sv.'_qsb_2010',
 	's'.$this->sv.'_qsc_2010',
 	's'.$this->sv.'_qsd_2010',
 	's'.$this->sv.'_qse_2010',
-	's'.$this->sv.'_qsf_2010',
+	's'.$this->sv.'_qsf_2010',//*/
 	's'.$this->sv.'_tblDataReview_2010',
 	's'.$this->sv.'_tblDataReviewTemp_2010',
 	);
@@ -86,14 +86,14 @@ foreach ($this->kod_produk as $myTable => $row)
 		if ($mula++%$lajur==0)
 		{ // mula kalau baki 0
 			?><td valign="top" align="left">
-<span class="badge badge-success"><?php echo $myTable ?></span><?php
+<span class="badge badge-success"><?php echo $myTable . '-' . $ID ?></span><?php
 			paparKodProduk($this->sv, $myTable, $row);
 			echo "</td>\n</tr>\n<tr>";
 		} // tamat kalau baki 0
 		else
 		{ // mula kalau baki bukan 0
 			?><td valign="top" align="left">
-<span class="badge badge-success"><?php echo $myTable ?></span><?php
+<span class="badge badge-success"><?php echo $myTable . '-' . $ID ?></span><?php
 			paparKodProduk($this->sv, $myTable, $row);
 			echo "</td>\n";
 		} // tamat kalau baki bukan 0

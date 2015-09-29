8c9293d5a912396b8da47524d0b9e323e564be20
Amin007 - Server <amin800507@gmail.com>
2015-09-29 20:19:14 +0800

    buang fungsi yang tiada kaitan

--- aplikasi/kawal/test.php
+++ aplikasi/kawal/test.php
@@ -10,14 +10,7 @@ class Test extends Kawal
 	
 	function index() 
 	{
-		$this->papar->mesej = 'Halaman ini tidak wujud';
-		$this->papar->baca('sesat/index');
-	}
-
-	function parameter() 
-	{
-		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
-		$this->papar->baca('sesat/index');
+		echo 'class Test::index() extends Kawal ';
 	}
 	
 ################################################################################################

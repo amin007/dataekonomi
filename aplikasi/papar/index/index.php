c156d7894902e4a775786aa62dc4c2808b98ca05
Amin007 - Server <amin800507@gmail.com>
2016-01-26 12:44:53 +0800

    buang jumbotron, cantum bawah class="row"

--- aplikasi/papar/index/index.php
+++ aplikasi/papar/index/index.php
@@ -17,8 +17,8 @@
 	</div>
 </div>
 
-<!-- Jumbotron -->
-<div class="bs-example"><div class="jumbotron">
+<div class="row">
+	<div class="col-sm-12">
 	<h1>Pengenalan</h1>
 	<p class="lead" style="text-align:justify;">
 	Ini adalah inovasi tentang BIG DATA syarikat di negeri johor serta yang
@@ -35,40 +35,38 @@
 	<a target="_blank" href="http://msic.stats.gov.my">MSIC</a> |
 	<a target="_blank" href="https://qss.stats.gov.my/qssv2/index.php">E-QSS</a> |
 	<a target="_blank" href="https://ejob.stats.gov.my/index.php">E-JOB</a> |
+	<a target="_blank" href="https://mail.stats.gov.my">Email</a>
 	<?php
 /*	<a target="_blank" href="http://exchserver.stats.gov.my/exchange">Email Stats</a> |
 	<a target="_blank" href="http://jp.stats.gov.my">JP@STATS</a>
-*/	?>
-	<a target="_blank" href="https://mail.stats.gov.my">Email</a>
-	</h5>
+*/	?></h5>
 	</p>
-</div></div>
-
-<!-- Example row of columns -->
-<div class="row">
-	<div class="col-lg-4">
-	<h2>Muar</h2>
-	<p>Kakitangan di pejabat operasi muar sila klik di bawah ini untuk daftar masuk<br>
-	<a class="btn btn-success" href="index/muar">
-	<span class="glyphicon glyphicon-arrow-right"></span>Daftar Masuk</a>
-	<a class="btn btn-success" href="../private_html">
-	<i class="glyphicon glyphicon-arrow-right"></i>M-Eco</a></p>
+	<div class="row">
+		<div class="col-lg-4">
+			<h2>Muar</h2>
+			<p>Kakitangan di pejabat operasi muar sila klik di bawah ini untuk daftar masuk<br>
+			<a class="btn btn-success" href="index/muar">
+			<span class="glyphicon glyphicon-arrow-right"></span>Daftar Masuk</a>
+			<a class="btn btn-success" href="../private_html">
+			<i class="glyphicon glyphicon-arrow-right"></i>M-Eco</a></p>
+		</div>
+		<div class="col-lg-4">
+			<h2>Johor Bahru</h2>
+			<p>Kakitangan di jabatan perangkaan malaysia negeri johor sila klik di bawah ini untuk daftar masuk<br>
+			<a class="btn btn-primary" href="index/jb">
+			<span class="glyphicon glyphicon-hand-right"></span>
+			Daftar Masuk</a></p>
+		</div>
+		<div class="col-lg-4">
+			<h2>Kluang</h2>
+			<p>Kakitangan di pejabat operasi kluang sila klik di bawah ini untuk daftar masuk<br>
+			<a class="btn btn-info" href="index/kluang">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			Daftar Masuk</a></p>
+		</div>
 	</div>
-	<div class="col-lg-4">
-	<h2>Johor Bahru</h2>
-	<p>Kakitangan di jabatan perangkaan malaysia negeri johor sila klik di bawah ini untuk daftar masuk<br>
-	<a class="btn btn-primary" href="index/jb">
-	<span class="glyphicon glyphicon-hand-right"></span>
-	Daftar Masuk</a></p>
-	</div>
-	<div class="col-lg-4">
-	<h2>Kluang</h2>
-	<p>Kakitangan di pejabat operasi kluang sila klik di bawah ini untuk daftar masuk<br>
-	<a class="btn btn-info" href="index/kluang">
-	<span class="glyphicon glyphicon-chevron-right"></span>
-	Daftar Masuk</a></p>
 	</div>
 </div>
-		  
+
 </div> <!-- /container -->
 <?php require 'bawah.php'; ?>

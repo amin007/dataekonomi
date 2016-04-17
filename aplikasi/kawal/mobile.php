663c06665178731fc40da574dec407d6e88f571b
Amin007 - Server <amin800507@gmail.com>
2016-04-17 14:07:07 +0800

    set pembolehubah utama

--- aplikasi/kawal/mobile.php
+++ aplikasi/kawal/mobile.php
@@ -18,6 +18,14 @@ class Mobile extends Kawal
 		
 	function icon() 
 	{
+		# Set pemboleubah utama
+		$this->papar->senaraiIP = dpt_ip(); # dapatkan senarai IP yang dibenarkan
+		$this->papar->ip = $ip = $_SERVER['REMOTE_ADDR'];
+		$this->papar->ip2 = substr($ip,0,10);
+		$this->papar->hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+		$this->papar->server = $_SERVER['SERVER_NAME'];
+		$this->papar->tajuk = 'Login Untuk POM';
+		
 		// pergi papar kandungan
 		$this->papar->baca('mobile/iconjqm');
 	}

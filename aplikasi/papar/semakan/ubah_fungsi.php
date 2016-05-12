bed66240e6a52d7b15c76104d67a0190f445ed17
Amin007 - Server <amin800507@gmail.com>
2016-05-12 12:22:23 +0800

    tambah fungsi analisis untuk ubah.php

---
+++ aplikasi/papar/semakan/ubah_fungsi.php
@@ -0,0 +1,79 @@
+<?php
+function analisis($perangkaan, $ppt, $jadual, $key, $data)
+{
+	$asetPenuh = $ppt['AsetPenuh'];
+	$asetBrgAm = $ppt['BrgAm'];
+	$sv = $perangkaan['sv'];
+	$hasil = $perangkaan['hasil'];
+	$belanja = $perangkaan['belanja'];
+	$aset = $perangkaan['aset'];
+	$sewa = $perangkaan['asetsewa'];
+	$noKey = substr($key, 0, 3);
+	//echo "<hr>$key : $sewaHarta ";
+	//echo "<hr>sv $sv | jadual $jadual ";
+	
+	if (in_array($key, array('thn','batch','Estab') ) )
+	{
+		$value = $data;
+	}
+	elseif ($sv=='205') 
+	{//untuk survey 205 sahaja
+		if ($jadual == 'q04_2010' && $noKey == 'F09') 
+			$nilai = ($sewa==0) ? 0 : (($data / $sewa) * 100);
+		elseif ($jadual == 'q04_2010' && $noKey != 'F09') 
+			$nilai = ($aset==0) ? 0 : (($data / $aset) * 100);
+		elseif ($jadual == 'q08_2010')
+			$nilai = ($hasil==0) ? 0 : (($data / $hasil) * 100 );
+		elseif ($jadual == 'q09_2010')
+			$nilai = ($belanja==0) ? 0 : (($data / $belanja) * 100 );
+		$name = 'name="' . $sv . '_' . $jadual . '[' . $key . ']"'
+			  . ' id="' . $key . '"';
+	}
+	elseif ($sv=='206') 
+	{//untuk survey 205 sahaja
+		if ($jadual == $sv.'_q04_2010' && $noKey == 'F09') 
+			$nilai = ($sewa==0) ? 0 : (($data / $sewa) * 100);
+		elseif ($jadual == $sv.'_q04_2010' && $noKey != 'F09') 
+			$nilai = ($aset==0) ? 0 : (($data / $aset) * 100);
+		elseif ($jadual == $sv.'_q08_2010')
+			$nilai = ($hasil==0) ? 0 : (($data / $hasil) * 100 );
+		elseif ($jadual == $sv.'_q09_2010')
+			$nilai = ($belanja==0) ? 0 : (($data / $belanja) * 100 );
+		$value = number_format($nilai,4,'.',',') . '%';
+		$name = 'name="' . $jadual . '[' . $key . ']"'
+			  . ' id="' . $key . '"';
+	}
+	elseif (in_array($sv,$asetPenuh)) 
+	{
+		$abaikan = array('s'.$sv.'_q02_2010','s'.$sv.'_q03_2010');
+		if ($jadual == 's'.$sv.'_q04_2010' && $noKey == 'F09') 
+			$nilai = ($sewa==0) ? 0 : (($data / $sewa) * 100);
+		elseif ($jadual == 's'.$sv.'_q04_2010' && $noKey != 'F09') 
+			$nilai = ($aset==0) ? 0 : (($data / $aset) * 100);
+		elseif ($jadual == 's'.$sv.'_q08_2010')
+			$nilai = ($hasil==0) ? 0 : (($data / $hasil) * 100 );
+		elseif ($jadual == 's'.$sv.'_q09_2010')
+			$nilai = ($belanja==0) ? 0 : (($data / $belanja) * 100 );
+		elseif(in_array($jadual,$abaikan))
+			$nilai = 'x';
+		$name = 'name="' . $jadual . '[' . $key . ']"'
+			  . ' id="' . $key . '"';
+	}
+	elseif (in_array($sv,$asetBrgAm)) 
+	{
+		if ($jadual == 's'.$sv.'_q02_2010')
+			$nilai = ($hasil==0) ? 0 : (($data / $hasil) * 100 );
+		elseif ($jadual == 's'.$sv.'_q03_2010')
+			$nilai = ($belanja==0) ? 0 : (($data / $belanja) * 100 );
+		elseif ($jadual == 's'.$sv.'_q04_2010')
+			$nilai = ($aset==0) ? 0 : (($data / $aset) * 100 );
+		$name = 'name="' . $jadual . '[' . $key . ']"'
+			  . ' id="' . $key . '"';
+	}
+	# istihar pembolehubah 
+	//$value = number_format($nilai,4,'.',',') . '%';
+	$value = ($nilai == 'x') ? '' : number_format($nilai,4,'.',',') . '%';
+	$input = '<input type="text" ' . $name . ' value="' 
+		   . $data . '" class="input-large">' . $value;
+	return '<td>' . $input . '</td>' . "\r";
+}

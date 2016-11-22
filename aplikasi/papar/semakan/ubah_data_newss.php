a904065dd27a5bc8122fa79eb6d180b62c23ece1
Amin007 - Server <amin800507@gmail.com>
2016-11-22 12:40:12 +0800

    1. buang koding if ( count($row)==0 ) 2. betulkan kedudukan koding bagi nampak cantik macam ku putih

--- aplikasi/papar/semakan/ubah_data_newss.php
+++ aplikasi/papar/semakan/ubah_data_newss.php
@@ -4,26 +4,18 @@
 	<ul class="nav nav-tabs">
 	<li class="active"><a href="#<?php echo $cari ?>" data-toggle="tab">
 	<span class="badge badge-success">Cari...</span></a></li>
-<?php 
-	foreach ($this->kawalID as $jadual => $baris):
-		if ( count($baris)==0 ): echo '';
-		else:?>
+<?php foreach ($this->kawalID as $jadual => $baris): ?>
 	<li><a href="#<?php echo $jadual; ?>" data-toggle="tab">
 	<span class="badge badge-success"><?php echo $jadual ?></span></a></li>
-<?php
-		endif;
-	endforeach;
-?>	</ul>
+<?php endforeach; ?>
+	</ul>
 <div class="tab-content">
 	<div class="tab-pane active" id="<?php echo $cari ?>">
 	<span class="badge badge-success">Mencari <?php echo $cari ?> ya</span>
 	</div>
 	<?php 
 foreach ($this->kawalID as $myTable => $row)
-{
-	if ( count($row)==0 ) echo '';
-	else
-	{?>	
+{?>	
 	<div class="tab-pane" id="<?php echo $myTable ?>">
 	<span class="badge badge-success">Anda berada di <?php echo $myTable ?></span>
 	<!-- Jadual <?php echo $myTable ?> ########################################### -->
@@ -33,13 +25,13 @@ foreach ($this->kawalID as $myTable => $row)
 	{# print the data row ?>
 		<td valign="top">
 		<table border="1" class="excel" id="example">
-	<?php foreach ( $row[$kira] as $key=>$data ) : 
-			$thn = ($key=='thn') ? $data : 2010; 
+		<?php foreach ( $row[$kira] as $key=>$data ) :
+			$thn = ($key=='thn') ? $data : 2010;
 			$keterangan = !isset($this->keterangan[$myTable][$key][$thn]) ?
 				'tiada maklumat' : $this->keterangan[$myTable][$key][$thn];
-			?><tr>
-		<td><abbr title="<?php echo  $keterangan ?>"><?php echo $key ?></abbr></td>
-		<td align="right"><?php echo (in_array($key, $senaraiMedan)) ? 
+		?><tr>
+		<td><abbr title="<?php echo $keterangan ?>"><?php echo $key ?></abbr></td>
+		<td align="right"><?php echo (in_array($key, $senaraiMedan)) ?
 			$data : BorangPapar::semakJenis($this->sv, $key, $data) ?></td>
 		<?php echo BorangPapar::inputText('kawal', $key, $data) ?>
 		</tr><?php endforeach ?>
@@ -51,7 +43,6 @@ foreach ($this->kawalID as $myTable => $row)
 	<!-- Jadual <?php echo $myTable ?> ########################################### -->
 	</div>
 <?php
-	} # if ( count($row)==0 )
 }
 ?>
 </div>

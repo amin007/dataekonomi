e5cca2e63f97ace99bace11044df2db7d1e52e2a
Amin007 - Server <amin800507@gmail.com>
2016-11-29 17:21:53 +0800

    tukar dari space kepada tab untuk fungsi Kebenaran::kawalKeluar()

--- aplikasi/kawal/cari.php
+++ aplikasi/kawal/cari.php
@@ -6,7 +6,7 @@ class Cari extends Kawal
 	public function __construct() 
 	{
 		parent::__construct();
-        Kebenaran::kawalKeluar();
+		Kebenaran::kawalKeluar();
 	}
 
 	public function index()

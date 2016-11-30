56c0cfa25b79862cbe284f8217b3e45a00cf28c0
Amin007 - Server <amin800507@gmail.com>
2016-11-30 13:18:50 +0800

    1. tambah #*** 2. buang space tab 3. buang fungsi xhrInsert(), xhrGetListings(), xhrDeleteListing()

--- aplikasi/kawal/ruangtamu.php
+++ aplikasi/kawal/ruangtamu.php
@@ -2,39 +2,23 @@
 
 class Ruangtamu extends Kawal 
 {
-
+#***************************************************************
 	function __construct() 
 	{
 		parent::__construct();
-		Kebenaran::kawalKeluar();	
-		//$this->papar->js = array('ruangtamu/js/default.js');
+		Kebenaran::kawalKeluar();
 	}
-	
+
 	function index() 
-	{	
+	{
 		$this->papar->baca('ruangtamu/index');
 	}
-	
+
 	function logout()
 	{
 		Sesi::destroy();
 		header('location: ' . URL);
 		exit;
 	}
-	
-	function xhrInsert()
-	{
-		$this->tanya->xhrInsert();
-	}
-	
-	function xhrGetListings()
-	{
-		$this->tanya->xhrGetListings();
-	}
-	
-	function xhrDeleteListing()
-	{
-		$this->tanya->xhrDeleteListing();
-	}
-
+#***************************************************************
 }

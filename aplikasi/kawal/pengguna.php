c4ea11256b3c50769a816c138f06d7819a621994
Amin007 - Server <amin800507@gmail.com>
2016-11-30 12:54:39 +0800

    1. tambah #*** 2. tukar space kepada tab pada fungsi Kebenaran::kawalKeluar 3. buang space tab 4. tukar // kepada #

--- aplikasi/kawal/pengguna.php
+++ aplikasi/kawal/pengguna.php
@@ -2,26 +2,26 @@
 
 class Pengguna extends Kawal 
 {
-
+#****************************************************************************************
 	public function __construct() 
 	{
 		parent::__construct();
-        Kebenaran::kawalKeluar();
+		Kebenaran::kawalKeluar();
 	}
-	
+
 	public function index() 
 	{	
 		$this->papar->senarai['pengguna'] = $this->tanya->senaraiPengguna();
 		$this->papar->baca('pengguna/index');
 	}
-	
+
 	public function tambahSimpan() 
 	{
 		$data = array();
 		$data['login'] = $_POST['login'];
 		$data['password'] = $_POST['password'];
 		$data['role'] = $_POST['role'];
-		
+
 		$semua = array('nama_pegawai');
 		foreach ($_POST as $myTable => $value)
 		{	
@@ -32,17 +32,17 @@ class Pengguna extends Kawal
 			}
 		}
 
-		// @TODO: Do your error checking!		
+		# @TODO: Do your error checking!
 		$this->tanya->tambahSimpan($data);
 		header('location: ' . URL . 'pengguna');
 	}
-	
+
 	public function ubah($id) 
 	{
 		$this->papar->pengguna = $this->tanya->seorangPengguna('no', $id);
 		$this->papar->baca('pengguna/ubah');
 	}
-	
+
 	public function ubahSimpan($id)
 	{
 		$data = array();
@@ -60,20 +60,21 @@ class Pengguna extends Kawal
 					$data[$myTable][$kekunci] = bersih($papar);
 			}
 		}
-		
-		// @TODO: Do your error checking!
+
+		# @TODO: Do your error checking!
 		$this->tanya->ubahSimpan($data);
 		header('location: ' . URL . 'pengguna');
 	}
-	
+
 	public function buang()
 	{
-		$this->papar->baca('pengguna/buang');	
+		$this->papar->baca('pengguna/buang');
 	}
-	
+
 	public function delete($id)
 	{
 		$this->tanya->delete($id);
 		header('location: ' . URL . 'pengguna');
 	}
+#****************************************************************************************
 }

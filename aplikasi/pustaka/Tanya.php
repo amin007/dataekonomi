061e78b3da58a8c5070a746d4801e6f0adfc6701
Amin007 - Server <amin800507@gmail.com>
2016-12-06 12:43:10 +0800

    tambah fungsi baru

--- aplikasi/pustaka/Tanya.php
+++ aplikasi/pustaka/Tanya.php
@@ -1,13 +1,32 @@
 <?php
 
-class Tanya 
+class Tanya
 {
-
-	function __construct() 
+##################################################################################################
+##------------------------------------------------------------------------------------
+	function __construct()
 	{
 		//$this->db = new PangkalanData(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 		$this->db = new DB_Mysqli(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 		//$this->crud = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 	}
+##------------------------------------------------------------------------------------
+	function ubahMedan($myTable, $medan)
+	{
+	/*
+	ALTER [IGNORE] TABLE tbl_name
+	MODIFY [COLUMN] col_name column_definition
+	[FIRST | AFTER col_name]
+	*/
+	
+		$sql = 'ALTER TABLE `' . $myTable . '` '
+			 . 'CHANGE `' . $medan['asal'] . '` '
+			 . '`' . $medan['baru'] . '` ' . $medan['jenis'] . ' '
+			 . 'AFTER `' . $medan['selepas'] . '` ';
 
+		echo '<pre>$sql->' . htmlentities($sql) . '</pre><br>';
+		
+	}
+##------------------------------------------------------------------------------------
+##################################################################################################
 }
